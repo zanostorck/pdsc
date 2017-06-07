aad97c90cd4f4045b1c9f6f5ae538754257f0340
zanostorck <matheuszs@gmail.com>
2017-06-07 02:52:00 -0300

    Em desenvolvimento

--- Lab5/lab5.m
+++ Lab5/lab5.m
@@ -1,1 +1,15 @@
 % Lab5
+% Define variaveis do workspace
+
+K = 100; % 100 N.m
+M1 = 1000; %1kg
+M2 = 1500; %1.5kg
+D = 52; %52 N.s/m
+
+% Plot das respostas de ambos os sistemas
+bgScale = 0.1;
+whitebg([bgScale bgScale bgScale])
+hold on;
+grid on;
+plot(tout,y(:,2),'cyan');    % G(s)
+plot(tout,y(:,3),'green');   % Degrau
