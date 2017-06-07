1673567a07bcc78724fb3c6ee2c09c09eff40ea9
zanostorck <matheuszs@gmail.com>
2017-06-07 17:42:47 -0300

    Pronto para apresentar

--- Lab5/lab5.m
+++ Lab5/lab5.m
@@ -2,14 +2,23 @@
 % Define variaveis do workspace
 
 K = 100; % 100 N.m
-M1 = 1000; %1kg
-M2 = 1500; %1.5kg
-D = 52; %52 N.s/m
+M1 = 1; %1kg
+M2 = 1.5; %1.5kg
+D_M = 52; %52 N.s/m
+
+A = [0 1 0 0;(-K/M1) (-D_M/M1) (K/M1) 0;0 0 0 1;(K/M2) 0 (-K/M2) 0];
+B = [0;0;0;(1/M2)];
+C = [0 0 0 1];
+D = [0];
+
+erro = y(:,2) - y(:,4);
 
 % Plot das respostas de ambos os sistemas
 bgScale = 0.1;
 whitebg([bgScale bgScale bgScale])
 hold on;
 grid on;
-plot(tout,y(:,2),'cyan');    % G(s)
-plot(tout,y(:,3),'green');   % Degrau
+plot(tout,y(:,2),'cyan','DisplayName','Space-state');    % State-space
+plot(tout,y(:,3),'yellow','DisplayName','Step');   % Step
+plot(tout,y(:,4),'red','DisplayName','Fluxo');     % Fluxo
+plot(tout,erro,'green','DisplayName','Erro entre SS e Fluxo');     % Erro
